bba6b64981f5d45c42377af44c8b658b32daf882
ariel <razariel2005@gmail.com>
2024-05-30 14:21:17 +0300

    Login Fix

--- PoliticallyForTeens/LoginActivity.cs
+++ PoliticallyForTeens/LoginActivity.cs
@@ -1,17 +1,33 @@
 ﻿
 using Android.App;
 using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using SQLite;
+using Android.Views;
+using Android.Content;
 
 namespace PoliticallyForTeens
 {
-    [Activity(Label = "LoginActivity")]
-    public class LoginActivity : Activity
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class LoginActivity : AppCompatActivity, Android.Views.View.IOnClickListener
     {
+        
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your application here
         }
+
+
+        public void OnClick(View v)
+        {
+            throw new System.NotImplementedException();
+        }
+
+
     }
 }
